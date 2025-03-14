7ae4a4aab63a27864b3324617b304dc284eb7ed6
Jacob Dodson <jacobcdodson@gmail.com>
2025-03-14 11:44:44 -0700

    open port changed from 3000 to 80

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const { createServer } = require('node:http');
 
 const hostname = '0.0.0.0';
-const port = 3000;
+const port = 80;
 
 const server = createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'application/json');
 
   const curTime = Math.floor(Date.now() / 1000);
-  const resData = {message:"Jacob Dodson",timestamp: curTime};
+  const resData = {message:"My name is Jacob Dodson",timestamp: curTime};
 
   res.end(JSON.stringify(resData));
 });
