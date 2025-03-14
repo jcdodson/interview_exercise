3b91fae386203e1aa6449eee3c6f9f487895968a
Jacob Dodson <jacobcdodson@gmail.com>
2025-03-14 12:16:10 -0700

    version added to json output

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 const { createServer } = require('node:http');
+const { readFileSync } = require('fs');
 
 const hostname = '0.0.0.0';
 const port = 80;
+
+const packageJson = JSON.parse(readFileSync('./package.json', 'utf8'));
+const ver = packageJson.version || "unknown";
 
 const server = createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'application/json');
 
   const curTime = Date.now();
-  const resData = {message:"My name is Jacob Dodson",timestamp: curTime};
+  const resData = {message:"My name is Jacob Dodson",timestamp:curTime,version:ver};
 
   res.end(JSON.stringify(resData));
 });
